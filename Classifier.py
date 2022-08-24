3f951f44cffdb54b4b9aef3d19e8c8c206bdb40e
sourabhrp151 <67378264+sourabhrp151@users.noreply.github.com>
2022-08-24 13:10:22 +0530

    Add files via upload

---
+++ Classifier.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+from time import time
+sys.path.append("../Mail_Classifier/")
+from Vectorizer import Vectorizer
+
+sys.path.append("../Mail_Classifier/")
+from New_Mail_Process import NVectorizer
+
+features_train, features_test, labels_train, labels_test = Vectorizer()
+
+features_new = NVectorizer()
+
+from sklearn import svm
+clf = svm.SVC(C=1, kernel='linear')
+
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 6), "s"
+
+from sklearn.metrics import accuracy_score
+pred1 = clf.predict(features_train)
+pred2 = clf.predict(features_test)
+print "Training Accuracy = ", accuracy_score(labels_train, pred1)
+print "Testing Accuracy = ", accuracy_score(labels_test,pred2)
+
+print "Category of new mail : ", clf.predict(features_new)
---
+++ New_Mail_Process.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+from nltk.stem.snowball import SnowballStemmer
+import string
+import os
+import sys
+import numpy
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def parseOutText(f):
+    
+
+    f.seek(0)  # go back to beginning of file
+    all_text = f.read()
+
+    # split off metadata
+    content = all_text.split("abcde")
+    words = ""
+    # remove punctuation
+    text_string = content[0].translate(string.maketrans("", ""), string.punctuation)
+
+    # split the text string into individual words, stem each word.
+    # space between each stemmed word
+    stemmer = SnowballStemmer("english")
+    split = text_string.split()
+    text = [stemmer.stem(word) for word in split]
+    words = ' '.join(text)
+
+    return words
+
+
+def main():
+    ff = open("../Mail_Classifier/New_Mail.txt", "r")
+    text = parseOutText(ff)
+    g = open("../Mail_Classifier/NewParsedtext.txt", "w")
+    g.write(text)
+    g.close()
+
+def NVectorizer() :
+
+    main()
+
+    f = open("../Mail_Classifier/NewParsedtext.txt", "r")
+
+    vectorizer = TfidfVectorizer(stop_words='english')
+
+    g = open("../Mail_Classifier/Parsedtext.txt", "r")
+    processed_input_train = vectorizer.fit_transform(g)
+    processed_input = vectorizer.transform(f)
+    
+    return processed_input
